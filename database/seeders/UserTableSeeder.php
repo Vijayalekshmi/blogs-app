bf2ed3f5dbafe77673da511d3586b0cf712b9805
Vijayalekshmi <j.vijayalekshmi@gmail.com>
2022-11-04 22:42:26 +0100

    Seeding user table

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use \App\Models\User;
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory(10)->create();
+    }
+}
