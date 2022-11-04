ae625c05b1958e4be3f6cb3968e2fc7df26b3b2c
Vijayalekshmi <j.vijayalekshmi@gmail.com>
2022-11-04 15:15:44 +0100

    handled user image to user

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function user_image()
+    {
+        return $this->hasOne(UserImage::class);
+    }
 }
