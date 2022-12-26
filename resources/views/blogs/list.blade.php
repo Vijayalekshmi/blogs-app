9a9f5bb0d590cbd7814826536de3a39aba221f50
Vijayalekshmi <j.vijayalekshmi@gmail.com>
2022-12-26 20:47:47 +0100

    changing the style of psot list page

--- resources/views/blogs/list.blade.php
+++ resources/views/blogs/list.blade.php
@@ -1,37 +1,30 @@
-<div class="p-6 bg-white border-b border-gray-200"> 
-                @if (count($posts) > 0)                   
-                    @foreach ($posts as $post)                                   
-                    <div class="row">
-                        <div class="col-md-8">
-                            <article id="">
-                                <header>
-                                    <div class="meta">
-                                        Written by <span class="author">{{ $post->user->name }} at </span>
-                                         <span class="date">{{  $post->created_at->format('d/m/Y') }}</span> 
-                                        |<span class="comments">
-                                            <span class="badge">{{ $post->comments->count() }}</span> Comments
-                                        </span>
-                                    </div>
-                                    <h2>
-                                        <a href="">
-                                        {{ $post->title }}</a></h2>
-                                    </header>
-                                    <div class="entry-content">
-                                        <a href="">
-                                            <img src="http://placehold.it/750x420" alt="" class="img-fluid" />
-                                        </a>
-                                        <p class="lead">{!!  \Illuminate\Support\Str::limit($post->content,150, $end='...') !!}  </p>
-
-                                        <a href="#" class="btn btn-primary">Read More <span class="glyphicon glyphicon glyphicon-chevron-right" aria-hidden="true"></span></a>
-                                    </div>
-                                <footer>
-                                </footer>
-                            </article>
-                        </div>
-                    </div>
-                  
-                    @endforeach
-                @else
-                    <p>No posts found</p>
-                @endif
-                </div>
+<div class="p-6 bg-white border-b border-gray-200">
+   @if (count($posts) > 0)                   
+   @foreach ($posts as $post)                                   
+   <div class="row">
+      <div class="col-md-8">
+         <h1 style="font-size: 20px;">
+            <b> {{ $post->title }} </b>
+         </h1>
+         <hr>
+         <br>
+         <div class="meta">
+            <span class="comments" style="float:right">
+            <span class="badge">{{ $post->comments->count() }}</span> Comments
+            </span>
+            <h5>
+               Post by {{ $post->user->name }} , {{  $post->created_at->format('d/m/Y') }}
+            </h5>
+         </div>
+         </span>
+         </span>
+         <p>{!!  \Illuminate\Support\Str::limit($post->content,150, $end='...') !!} </p>
+         <a href="{{ route('posts.show', ['post' => $post]) }}" class="btn btn-primary">Read More <span class="glyphicon glyphicon glyphicon-chevron-right" aria-hidden="true"></span></a>
+         <br><br>
+      </div>
+   </div>
+   @endforeach
+   @else
+   <p>No posts found</p>
+   @endif
+</div>
