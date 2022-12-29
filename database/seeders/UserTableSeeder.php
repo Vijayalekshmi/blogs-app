2320a7c7f93104cba6f183a9f95ddcd69dc9c694
Vijayalekshmi <j.vijayalekshmi@gmail.com>
2022-12-29 16:42:26 +0100

    adding user role to user table

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use \App\Models\User;
+use Illuminate\Support\Str;
+
 class UserTableSeeder extends Seeder
 {
     /**
@@ -15,5 +17,13 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         User::factory(10)->create();
+        $user = new User;
+        $user->name = 'admin';
+        $user->email = 'admin@gmail.com';
+        $user->role='admin';
+        $user->email_verified_at = now();
+        $user->password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'; // password
+        $user->remember_token = Str::random(10);       
+        $user->save();
     }
 }
