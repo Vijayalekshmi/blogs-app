d2c8f70f386f67d6907553119b87ea403da9e4ef
Vijayalekshmi <j.vijayalekshmi@gmail.com>
2022-12-31 17:07:02 +0100

    adding new logo to the app

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                    <img src="{{ asset('images/new-logo.png') }}" height=50 width=50 alt="Logo">                      
                     </a>
                 </div>
 
